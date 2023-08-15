c96a71bab1628ec18fd8949da7d4f987f8b8a92d
MShkut <111993881+MShkut@users.noreply.github.com>
2023-08-15 12:13:39 -0400

    Add files via upload

---
+++ complete_ws/drone_control/launch/offboard_aruco_both.launch.py
@@ -0,0 +1,15 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    offboard_aruco_both_launch = Node(
+        package='drone_control',
+        executable='offboard_aruco_both'
+    )
+
+    return LaunchDescription([
+        offboard_aruco_both_launch
+    ])
